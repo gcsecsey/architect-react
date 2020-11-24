698c89edd6d48c382a58d20b1baf80ed61f6c744
Gergely Csecsey <gcsecsey@gmail.com>
2020-11-25 00:13:44 +0100

    add amplify

---
+++ arc-app/src/aws-exports.js
@@ -0,0 +1,12 @@
+const config = {
+  region: "eu-west-1",
+  aws_cloud_logic_custom: [
+    {
+      name: "arc-api",
+      endpoint: "",
+      region: "eu-west-1",
+    },
+  ],
+};
+
+export default config;
